14e21859c1b19db9661fcc4a5683200c2f422b3c
Iryna Malash <mal7rish@gmail.com>
2023-06-03 21:16:31 +0500

    Update filter by genres

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const GENRES = ['Action', 'Comedy', 'Crime', 'Fantastic'];
+export const GENRES = ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama',
+  'Family', 'Fantasy', 'Mystery', 'Romance', 'Science Fiction', 'Thriller'];
+export const FILTER_GENRES = ['Action', 'Comedy', 'Crime', 'Drama', 'Fantasy'];
 export const SORT_BY = ['Release date', 'Genre', 'Rating'];
 
 export const KEYS = {
